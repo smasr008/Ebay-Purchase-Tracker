342aecdd001866c4cec00d462153829f7618d665
Avery <147835581+avery-codes@users.noreply.github.com>
2024-07-20 12:22:25 -0700

    Testing
    Testing

---
+++ Ebay Purchase Tracker/Ebay Purchase Tracker/Ebay_Purchase_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ebay_Purchase_TrackerApp.swift
+//  Ebay Purchase Tracker
+//
+//  Created by Avery Robbins on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Ebay_Purchase_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
