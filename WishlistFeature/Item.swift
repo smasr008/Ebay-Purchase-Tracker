5238936d1220666af448c2dc6b630a34b5b4e621
AsjaJennings <89799691+AsjaJennings@users.noreply.github.com>
2024-07-24 10:33:00 -0400

    Update Item.swift

--- WishlistFeature/Item.swift
+++ WishlistFeature/Item.swift
@@ -4,12 +4,12 @@
 //
 // 
 //
-
 import Foundation
 
 struct Item: Identifiable {
     let id = UUID()
     let name: String
     let imageName: String  
+    let price: Double
     var isLiked: Bool
 }
